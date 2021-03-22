fa4d88be82432fe16b708865a234d8363abd30a3
redaphid <iam@aaronherres.com>
2021-03-21 17:30:50 -0700

    bad json

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 const bs58check = require('bs58check')
 
+const readline = require('readline');
+
+const r = readline.createInterface({
+      input: process.stdin,
+
+})
+
+function chk(addr){
+
+    try {
+       console.log(JSON.stringify({addr, len: addr.length}),',')
+       bs58check.decode(addr)
+        console.log(addr)
+    } catch (e){
+        //console.log(addr, e)
+    }
+}
+console.log('[')
+
+r.on('line', chk)
+r.on('close', ()=>{console.log('{}]')})
+/*
 process.stdin.on('readable', () => {
         let chunk;
     while ((chunk = process.stdin.read()) !== null) {
@@ -10,4 +32,4 @@ process.stdin.on('readable', () => {
            } catch (e) {
            }
     };
-})
+})*/
